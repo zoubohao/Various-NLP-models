37a6b23b6ec8ae862a0529e234ab374fb863ed66
Bhao.Zzo <35212019+zoubohao@users.noreply.github.com>
2019-06-20 21:36:15 +0800

    Add files via upload

---
+++ PythonFeaturesTest.py
@@ -0,0 +1,73 @@
+
+### Test Class with __call__ method.
+### In generally, it can add () after the object of this class
+### and run the python code in its function.
+class Test (object) :
+
+    def __init__(self):
+        pass
+
+    def __call__(self, a , b):
+        return a + b
+
+test = Test()
+print("###############")
+print("Test call function.")
+print(test(1.2,5.1))
+print("###############")
+
+
+### Test decorator
+### The decorator in python is to dissolve the problem, code redundancy.
+### The kernel code is the object which we want to decorate.
+### By using the decorator, we can add additional feature around the kernel code
+### but without writing another code once by once.
+
+### In this form, The function TestDecorator can not use @ symbol to decorate
+### the kernel code. Only can deliver one of object of kernel function to complete
+### logical.
+def TestDecorator(func,ifOutputName):
+    def wrapper(*args, **kwargs):
+        print("This is an information before starting using function.")
+        if ifOutputName:
+            print("The parameter ifOutputName is True.")
+            print("The name of this function is ", func.__name__)
+        else:
+            print("The parameter ifOutputName is False.")
+        return func(*args, **kwargs)
+    return wrapper
+
+def TestFunc(a,b,ifAdd = True):
+    if ifAdd:
+        return a + b
+    else:
+        return a * b
+test = TestDecorator(TestFunc,True)(1,2.0,False)
+print(test)
+print("#################")
+### In this form, The TestDecorator1 only has one parameter.
+### It can use @ symbol to substitute the assignment code:
+### test = TestDecorator(TestFunc,True)(1,2.0,False)
+def TestDecorator1(ifOutputName) :
+    def decorator(func) :
+        def wrapper(*args ,  **kwargs) :
+            print("This is an information before starting using function.")
+            if ifOutputName:
+                print("The parameter ifOutputName is True.")
+                print("The name of this function is ", func.__name__)
+            else:
+                print("The parameter ifOutputName is False.")
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+@TestDecorator1(False)
+def TestFunc1(a,b,ifAdd = True):
+    if ifAdd:
+        return a + b
+    else:
+        return a * b
+print(TestFunc1(1,2.0,True))
+
+
+
